550dfe58c110775b11a61144d6af2b9adad24645
Ayush Dixit <fsayush100@gmail.com>
2025-11-09 10:23:47 +0530

    Add node-pg-migrate for database migrations and update package scripts

---
+++ src/migrations/1762663068887_users-table.sql
@@ -0,0 +1,13 @@
+-- Up Migration
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    image_url VARCHAR(255) NOT NULL
+);
+
+-- Down Migration
+DROP TABLE users;
